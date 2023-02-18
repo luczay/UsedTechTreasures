f02f8e6e19045f7f08e3b43f20e3c2282ab847c8
Luczay-Mazula Adolf <luczay1999@gmail.com>
2023-02-18 16:53:21 +0100

    Add attemt_repetitively() static method to attempt scraping a data repeatedly within a specified time frame

--- scrape.py
+++ scrape.py
@@ -1,12 +1,14 @@
-from abc import ABC, abstractmethod
 import pandas as pd
+import constants
+from abc import ABC, abstractmethod
 from typing import Union
-
+from time import sleep
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import TimeoutException
 
 
 class Scrape(ABC):
@@ -46,3 +48,29 @@ class Scrape(ABC):
             return driver.find_elements(By.XPATH, select)
         else:
             return WebDriverWait(driver, wait).until(ec.presence_of_element_located((By.XPATH, select)))
+
+    @staticmethod
+    def _attempt_repetitively(driver: webdriver, selector_type: str,
+                              select_str: str, init_time=1, steps=1, end_time=6):
+        if not any(constants.selector_types):
+            raise Exception('Wrong selector type!')
+
+        data = ''
+        time = init_time
+        while (data is None or data == '') and time < end_time:
+            sleep(time)
+
+            try:
+                if selector_type == 'xpath':
+                    data = Scrape._find_by_xpath(driver, select_str, wait=4).text
+                else:
+                    data = Scrape._find_by_css(driver, select_str, wait=4).text
+            except TimeoutException:
+                pass
+
+            time += steps
+
+        if time == end_time and data is None or data == '':
+            data = ''
+
+        return data
